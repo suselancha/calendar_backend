fc8518e0d44dd50fc5096562871c11d0535735e9
Alfredo <dominiolibre@gmail.com>
2020-11-04 13:03:13 -0300

    Fin seccion 24

--- controllers/events.js
+++ controllers/events.js
@@ -1,12 +1,17 @@
 const { response } = require('express');
 const { generarJWT } = require('../helpers/jwt');
+const { eventNames } = require('../models/Evento');
 const Evento = require('../models/Evento');
 
-const getEventos = (req, res = response) => {
+const getEventos = async(req, res = response) => {
+
+    const eventos = await Evento.find()
+                                .populate('user', 'name');
+                                //.populate('user', 'name password');
 
     res.json({
         ok:true,
-        msg: 'getEventos'
+        eventos
     })
 }
 
@@ -37,21 +42,96 @@ const crearEvento = async(req, res = response) => {
 
 }
 
-const actualizarEvento = (req, res = response) => {
+const actualizarEvento = async(req, res = response) => {
 
-    res.json({
-        ok:true,
-        msg: 'actualizarEvento'
-    })
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        // Verifico que exista el evento
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe'
+            });
+        }
+
+        // Verifico que sea el mismo usuario que creo el evento que lo quiere modificar
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios de editar este evento'
+            });
+        }
+
+        // Obtengo la nueva data
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        //Actualiza, pero devuelve el objeto sin actualizar
+        //const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento);
+        //Actualiza, y devuelve el objeto actualizado, con la ultima opcion
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 
 }
 
-const eliminarEvento = (req, res = response) => {
+const eliminarEvento = async(req, res = response) => {
 
-    res.json({
-        ok:true,
-        msg: 'eliminarEvento'
-    })
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        // Verifico que exista el evento
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe'
+            });
+        }
+
+        // Verifico que sea el mismo usuario que creo el evento que lo quiere modificar
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios de eliminar este evento'
+            });
+        }
+
+        await Evento.findByIdAndDelete(eventoId);
+
+
+        res.json({
+            ok: true
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 
 }
 
--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -6,7 +6,7 @@ const validarJWT = (req, res=response, next) => {
 
     //x-token en los HEADERS
     const token = req.header('x-token');
-    console.log(token);
+    //console.log(token);
 
     if ( !token ) {
         return res.status(401).json({
